44ba8c17b847a6698ccaa342c2d2f20495eb3135
GwydionDesert <desert.gwydion@gmail.com>
2017-04-06 15:05:29 +0200

    setting up Unity Project

---
+++ Assets/onClick.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onClick : MonoBehaviour {
+
+    GameObject lastHit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        {
+            // get mouse position to world space
+            Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
+
+            // on hit
+            if (hit)
+            {
+                lastHit = hit.transform.gameObject;
+                lastHit.GetComponent<SpriteRenderer>().color = Color.green;
+                Debug.Log(hit.transform.name);
+                Debug.Log( hit.transform.gameObject);
+            }
+            else
+            {
+                lastHit.GetComponent<SpriteRenderer>().color = Color.white;
+            }
+        }
+    }
+}
