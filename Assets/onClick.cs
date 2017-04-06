fc2ce93df1b7020a9911118a5d4bedcb7af57f1e
GwydionDesert <desert.gwydion@gmail.com>
2017-04-06 15:32:31 +0200

    mouseover working

--- Assets/onClick.cs
+++ Assets/onClick.cs
@@ -6,29 +6,36 @@ public class onClick : MonoBehaviour {
 
     GameObject lastHit;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
 	void Update () {
         {
             // get mouse position to world space
             Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
             RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
 
-            // on hit
-            if (hit)
+            Debug.DrawRay(new Vector3 (0,0,0), rayPos);
+
+            // check if object has a sprite renderer
+            // mostly only called because of else block
+            try
             {
-                lastHit = hit.transform.gameObject;
-                lastHit.GetComponent<SpriteRenderer>().color = Color.green;
-                Debug.Log(hit.transform.name);
-                Debug.Log( hit.transform.gameObject);
+                // on mouse over
+                if (hit)
+                {
+
+                    lastHit = hit.transform.gameObject;
+                    lastHit.GetComponent<SpriteRenderer>().color = Color.red;
+                    Debug.Log(hit.transform.name);
+                    Debug.Log(hit.transform.gameObject);
+
+                }
+                else
+                {
+                    lastHit.GetComponent<SpriteRenderer>().color = Color.white;
+                }
             }
-            else
+            catch
             {
-                lastHit.GetComponent<SpriteRenderer>().color = Color.white;
+                //Debug.Log("no SpriteRenderer found");
             }
         }
     }
